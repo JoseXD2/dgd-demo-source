067a05cec40053f1d7c5a1ef205539b0adaa40de
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-16 09:56:02 -0300

    Update AchievementsMenuState.hx

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -68,7 +68,11 @@ class AchievementsMenuState extends MusicBeatState
 		descText.borderSize = 2.4;
 		add(descText);
 		changeSelection();
-
+		
+                #if android
+	        addVirtualPad(UP_DOWN, B);
+                #end
+			
 		super.create();
 	}
 
